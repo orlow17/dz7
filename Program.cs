851c085c7a92ff490b10507fe18925a6d54380f0
Vladimir <wl.orlow_758@mail.ru>
2024-04-20 01:33:50 +0300

    added task1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// TASK 1
+string Sum(int start, int stop)
+{
+    if (start == stop) return Convert.ToString(start);
+    return start + " " + Sum(start + 1, stop);
+}
+
+Console.WriteLine("Введите начальное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write(Sum(n,m));
