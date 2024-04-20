62eb1e5af8bc58f0ea8ff97da89be0be141187d5
Vladimir <wl.orlow_758@mail.ru>
2024-04-20 18:04:46 +0300

    added task 3

--- Program.cs
+++ Program.cs
@@ -12,16 +12,47 @@
 // Console.Write(Sum(n,m));
 
 // TASK 2
-int Akk(int m, int n)
+// int Akk(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return Akk(m-1, 1);
+//     else return Akk(m-1, Akk(m, n-1));
+// }
+
+// Console.WriteLine("Введите число m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write(Akk(m,n));
+
+// TASK 3
+int [] Random(int[] arr)
 {
-    if (m == 0) return n + 1;
-    else if (n == 0) return Akk(m-1, 1);
-    else return Akk(m-1, Akk(m, n-1));
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
 }
-
-Console.WriteLine("Введите число m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число n: ");
+void PrintArr(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}\t");
+    }
+    Console.WriteLine();
+}
+void Str ( int[] arr, int i )
+{
+if (i >= 0)
+{
+    Console.Write(arr[i] + " ");
+    Str(arr, i - 1); 
+}
+}
+Console.Write("Введите размерность массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write(Akk(m,n));
-
+int [] arr = new int[n];
+Random(arr);
+PrintArr(Random(arr));
+Str(arr, n - 1);
